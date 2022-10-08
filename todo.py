341159cdb45b22f766216ed88b64f7839dde5a6c
Enthu-Coder <wiprosweta1995@gmail.com>
2022-10-09 00:03:29 +0530

    Implemented put-update-delete

--- todo.py
+++ todo.py
@@ -1,13 +1,49 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Query
+from model import Todo, TodoItem
+
+
 todo_router = APIRouter()
 
 todo_list = []
 
 @todo_router.post("/todo")
-async def add_todo(todo: dict) -> dict:
+async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {"message": "Todo added successfully"}
 
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
     return {"todos": todo_list}
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title="The ID of the Todo to retrieve")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return{
+                "todo": todo
+            }
+    return{
+        "message": "Todo with supply ID doesn't exist"
+    }
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated")):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return{
+                "message": "Todo updated successfully"
+            }
+    return {
+        "message": "Todo with ID doesn't exist"
+    }
+            
+@todo_router.delete("/todo")
+async def delete_all_todo() -> dict:
+    todo_list.clear()
+    return{
+        "message": "Todo deleted successfully"
+    }
+
+@todo_router.get("/todo")
+async def query_route(query: str = Query(None)):
+    return query
